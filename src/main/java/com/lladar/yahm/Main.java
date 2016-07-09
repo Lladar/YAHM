d11634e8754629cefdac30360c906f05269d9a74
Brian Byrkett <brian.byrkett@outlook.com>
2016-07-08 23:26:21 -0700

    Created proxy files & added to Main.java

--- src/main/java/com/lladar/yahm/Main.java
+++ src/main/java/com/lladar/yahm/Main.java
@@ -1,9 +1,10 @@
 package com.lladar.yahm;
 
-import net.minecraftforge.fml.common.Mod;
+import com.lladar.yahm.proxy.Common;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -19,21 +20,27 @@ public class Main {
     @Instance
     public static Main instance = new Main();
 
+    @SidedProxy(clientSide="com.lladar.yahm.proxy.Client", serverSide="com.lladar.yahm.proxy.Server")
+    public static Common proxy;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent e) {
         // Pre-Initialization step for items / blocks
+        this.proxy.preInit(e);
         System.out.println("YAHM Pre-Initialized");
     }
 
     @EventHandler
     public void init(FMLInitializationEvent e) {
         // Initialization step for recipes
+        this.proxy.init(e);
         System.out.println("YAHM Initialized");
     }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent e) {
         // Post-Initialization step for mod compatibility
+        this.proxy.postInit(e);
         System.out.println("YAHM Post-Initialized");
     }
 }
