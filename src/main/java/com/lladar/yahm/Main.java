5d3722205cc7437ce7c58489ac02d1a098762530
Brian Byrkett <brian.byrkett@outlook.com>
2016-07-08 23:06:10 -0700

    Created Main

--- src/main/java/com/lladar/yahm/Main.java
+++ src/main/java/com/lladar/yahm/Main.java
@@ -1,4 +1,39 @@
 package com.lladar.yahm;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+
+@Mod(modid =Main.MODID, name = Main.MODNAME, version = Main.VERSION)
 public class Main {
+
+    public static final String MODID = "YAHM";
+    public static final String MODNAME = "Yet Another Harvest Mod";
+    public static final String VERSION = "0.1";
+
+    @Instance
+    public static Main instance = new Main();
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent e) {
+        // Pre-Initialization step for items / blocks
+        System.out.println("YAHM Pre-Initialized");
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent e) {
+        // Initialization step for recipes
+        System.out.println("YAHM Initialized");
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent e) {
+        // Post-Initialization step for mod compatibility
+        System.out.println("YAHM Post-Initialized");
+    }
 }
